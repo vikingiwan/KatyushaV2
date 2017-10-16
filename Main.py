2b6490245f707261612bbeb4756c5572f330d766
BuildTools <vikingiwan@gmail.com>
2017-10-15 21:16:39 -0400

    added info command to userCommands list

--- Main.py
+++ Main.py
@@ -19,7 +19,7 @@ bot = commands.Bot(command_prefix="!")
 connection = sqlite3.connect('KatyushaData.db')
 cur = connection.cursor()
 killResponses = ["%s 'accidentally' fell in a ditch... RIP >:)", "Oh, %s did that food taste strange? Maybe it was.....*poisoned* :wink:", "I didn't mean to shoot %s, I swear the gun was unloaded!", "Hey %s, do me a favor? Put this rope around your neck and tell me if it feels uncomfortable.", "*stabs %s* heh.... *stabs again*....hehe, stabby stabby >:D", "%s fell into the ocean whilst holding an anvil...well that was stupid."]
-userCommands = ["test", "hug", "pat", "roll", "remind", "kill", "calc", "addquote", "quote", "joke", "dirtyjoke", "pfp", "version"]
+userCommands = ["test", "hug", "pat", "roll", "remind", "kill", "calc", "addquote", "quote", "joke", "dirtyjoke", "pfp", "info", "version"]
 operatorCommands = ["say", "purge", "getBot"]
 
 #Remove default help command
